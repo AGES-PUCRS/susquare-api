915690f90906176183e8187ff0d912228d041bb8
Cássio Trindade <cassiowt@gmail.com>
2016-10-08 13:35:12 -0300

    definido a url do mongo db

--- development/config.js
+++ development/config.js
@@ -1,4 +1,9 @@
+/* 
+ HOMO = 'mongodb://10.32.223.4/susquare'
+ PROD = 'mongodb://10.32.223.7/susquare'
+*/
+
 export default {
-    databaseUrl: 'mongodb://localhost/susquare',
+    databaseUrl: process.env.URL_MONGO || 'mongodb://10.32.223.4/susquare' ,
     port: process.env.PORT || 8888
 }
