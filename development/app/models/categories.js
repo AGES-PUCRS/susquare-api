1cd543e23f68beed33cc6a7ed99f183aa696f915
Max Zorzetti <maximilian.zorzetti@acad.pucrs.br>
2016-10-08 10:11:49 -0300

    Categories list added to /models

---
+++ development/app/models/categories.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        "descricaoCategoria":"HOSPITAL"
+    },
+    {
+        "descricaoCategoria":"POSTO DE SAÚDE"
+    },
+    {
+        "descricaoCategoria":"URGÊNCIA"
+    },
+    {
+        "descricaoCategoria":"SAMU"
+    },
+    {
+        "descricaoCategoria":"FARMÁCIA"
+    },
+    {
+        "descricaoCategoria":"CLÍNICA"
+    },
+    {
+        "descricaoCategoria":"CONSULTÓRIO"
+    },
+    {
+        "descricaoCategoria":"LABORATÓRIO"
+    },
+    {
+        "descricaoCategoria":"APOIO À SAÚDE"
+    },
+    {
+        "descricaoCategoria":"ATENÇÃO ESPECÍFICA"
+    },
+    {
+        "descricaoCategoria":"UNIDADE ADMINISTRATIVA"
+    },
+    {
+        "descricaoCategoria":"ATENDIMENTO DOMICILIAR"
+    }
+]
