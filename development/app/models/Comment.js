bb2d5ac12029296e55a4a22b308764c97ceeeccb
CARLOS HENRIQUE ARYPE ILHA <15104291@pucrs.br>
2016-10-28 18:04:04 -0200

    testecomment

---
+++ development/app/models/Comment.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+const model = mongoose.model('Comment', {
+     idAtendimento: String,
+    tipoComentario: String, // C=Crítica, E=elogio como arruma isso?
+    comentario: String,
+	timestampComentario: String,   //date?????????
+	imagens: [String]; /////como faz?
+});
+
+export default model
